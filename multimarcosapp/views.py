069356668eac9d1d806cc6273cb10452420e09d9
Luis <quintero.geb@outlook.com>
2017-11-27 13:32:27 -0600

    EStado inicial del proyecto cuando todavia no agrego plantillas django al sitio

---
+++ multimarcosapp/views.py
@@ -0,0 +1,13 @@
+from django.shortcuts import render
+
+def index(request):
+    context={}
+    return render(request,"index.html",context)
+
+def about(request):
+    context={}
+    return render(request,"about.html",context)
+
+def contact(request):
+    context={}
+    return render(request,"contact.html",context)
